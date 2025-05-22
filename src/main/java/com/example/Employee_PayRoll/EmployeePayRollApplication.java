5662a1282299d4d630a312115c7d6e70ceb6a7d1
98601700+Shrinath-M@users.noreply.github.com <98601700+Shrinath-M@users.noreply.github.com>
2025-05-22 10:50:37 +0530

    UC1_Create_Spring_Project

---
+++ src/main/java/com/example/Employee_PayRoll/EmployeePayRollApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee_PayRoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayRollApplication.class, args);
+	}
+
+}
